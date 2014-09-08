c268ff3c91b6677631a3c32975c65445d4442a9e
Tom Shea <tom@shea.at>
2014-09-08 15:15:19 +0000

    Fix different context and add giantbomb faucet

--- faucets.js
+++ faucets.js
@@ -15,14 +15,17 @@ exports.getYoutubeID = require('get-youtube-id');
 exports.parseURL = function(inputURL) {
   var parsed = url.parse(inputURL);
   var data = {};
-  log(parsed.host);
   if(parsed.host == "youtube.com" || parsed.host == "www.youtube.com") {
     data.faucet = "youtube";
-    data.contentId = faucets.getYoutubeID(inputURL);
+    data.contentId = exports.getYoutubeID(inputURL);
   }
   if(parsed.host == "mediacru.sh" || parsed.host == "www.mediacru.sh") {
     data.faucet = "mediacrush";
-    data.contentId = faucets.getMediaCrushID(inputURL); 
+    data.contentId = exports.getMediaCrushID(inputURL); 
+  }
+  if(parsed.host == "v.giantbomb.com") {
+    data.faucet = "giantbomb";
+    data.contentId = inputURL;
   }
 
   return data;
